9512f695a4fe164e1a8024b5d9c95aec7b1a6b6e
palakk-48 <palakg1948@gmail.com>
2026-07-19 19:10:50 +0530

    created love calculator

---
+++ randomNumGenerate.js
@@ -0,0 +1,21 @@
+//random number generator
+var n= Math.random();
+console.log(n);
+
+
+//random number generator between 0 and 100
+var n= Math.random();
+n=n*100;
+console.log(n);
+
+
+//random number generator between 0 and 100 rounded
+var n= Math.random();
+n=n*100;
+console.log(Math.round(n));
+
+
+//randome number generator for dice
+var n= Math.random();
+n=n*6;
+console.log(Math.floor(n)+1);
